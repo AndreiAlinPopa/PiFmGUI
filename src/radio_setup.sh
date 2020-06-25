7712415304221df381f27ca7db375685bd9845fd
AndreiAlinPopa <62486671+AndreiAlinPopa@users.noreply.github.com>
2020-06-25 17:38:21 +0100

    Fixed Path Label

--- src/radio_setup.sh
+++ src/radio_setup.sh
@@ -1,4 +1,4 @@
 sudo apt-get install libsndfile1-dev
 sudo echo "gpu_freq=250" >> /boot/config.txt
-sudo make -C /home/pi/PiFmAdv/src clean
-sudo make -C /home/pi/PiFmAdv/src
+sudo make -C /home/pi/PiFmGUI/src clean
+sudo make -C /home/pi/PiFmGUI/src
